86bcd989270c9d37c672b75e64c760c8620b8bc1
thomasgerritsen-code <thomasgerritsen@outlook.com>
2026-09-01 21:27:10 +0200

    feat: add DealHunter mobile service worker

---
+++ docs/sw.js
@@ -0,0 +1,17 @@
+const CACHE='dealhunter-shell-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  const url=new URL(event.request.url);
+  if(event.request.method!=='GET') return;
+  if(url.pathname.includes('/data/')){
+    event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
+    return;
+  }
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
+});
